879d72d649201873e58a0d050b54fa7e75d52f0d
Hemanth Kumar <avisihks@gmail.com>
2023-09-13 15:47:34 +0530

    Implementation using CRUD CRUD

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import CandyForm from "./components/candyForm/candyForm";
 import CandyTable from "./components/candyTable/candyTable";
+import axios from "axios";
+
+const URL = "https://crudcrud.com/api/57685d4120ab4b1a87417be1d1d1dd85/candies";
+
+const addCandy = async (url, payload) => {
+  try {
+    const response = await axios.post(url, payload);
+    const data = await response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateCandiesQuantity = async (url, lessBy, id) => {
+  try {
+    const initial_candy_response = await axios.get(`${url}/${id}`);
+    const initial_candy = initial_candy_response.data;
+    console.log({ initial_candy });
+    initial_candy.quantity = parseInt(initial_candy.quantity) - lessBy;
+    console.log(initial_candy);
+    const response = await axios.put(`${url}/${id}`, initial_candy);
+    console.log({ response });
+    const final_candy = await axios.get(`${url}/${id}`);
+    console.log({ final_candy });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 function App() {
   const [candyData, setCandyData] = useState([]);
 
-  const handleAddCandy = (newCandy) => {
-    setCandyData([...candyData, newCandy]);
+  useEffect(() => {
+    const fetchCandies = async () => {
+      try {
+        const response = await axios.get(URL);
+        const data = response.data;
+        setCandyData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCandies();
+  }, []);
+
+  const handleAddCandy = async (newCandy) => {
+    const updatedCandyData = await addCandy(URL, newCandy);
+    setCandyData([...candyData, updatedCandyData]);
   };
 
   return (
     <div className="App">
       <h1>Candy Shop</h1>
       <CandyForm onAddCandy={handleAddCandy} />
-      {candyData.length ? <CandyTable candyData={candyData} /> : ""}
+      {candyData.length ? (
+        <CandyTable
+          candyData={candyData}
+          updateCandiesQuantity={updateCandiesQuantity}
+          URL={URL}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
--- src/components/candyTable/candyTable.js
+++ src/components/candyTable/candyTable.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-function CandyTable({ candyData }) {
+function CandyTable({ candyData, updateCandiesQuantity, URL }) {
+  const buyOneHandler = (event) => {
+    event.preventDefault();
+    const candy_id = event.target.parentElement.parentElement.id;
+    updateCandiesQuantity(URL, 1, candy_id);
+  };
   return (
     <table>
       <thead>
@@ -12,12 +17,21 @@ function CandyTable({ candyData }) {
         </tr>
       </thead>
       <tbody>
-        {candyData.map((candy, index) => (
-          <tr key={index}>
+        {candyData.map((candy) => (
+          <tr key={candy._id} id={candy._id}>
             <td>{candy.candyName}</td>
             <td>{candy.description}</td>
             <td>${candy.price}</td>
             <td>{candy.quantity}</td>
+            <td>
+              <button onClick={buyOneHandler}>BuyOne</button>
+            </td>
+            <td>
+              <button>Buy2</button>
+            </td>
+            <td>
+              <button>Buy3</button>
+            </td>
           </tr>
         ))}
       </tbody>
